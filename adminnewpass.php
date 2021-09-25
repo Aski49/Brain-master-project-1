485577784c4652e80ec717c0cb6ba0e246650284
Aski49 <88615751+Aski49@users.noreply.github.com>
2021-09-25 14:01:15 +0530

    Add files via upload

--- adminnewpass.php
+++ adminnewpass.php
@@ -1,31 +1,24 @@
 <?php
-
+$email = $_POST['email'];
 $password = $_POST['password'];
-$password = $_POST['cpassword'];
+$cpassword = $_POST['cpassword'];
 
 $conn = new mysqli("localhost","root","","brain master");
 
 if ($conn->connect_error) {
     die("connection failed :".$conn->connect_error);
 }
-else{
-    $stmt = $conn->prepare("update admin set password =?, cpassword =? where email =?");
-    $stmt ->bind_param("ss",$password,$cpassword);
-    $stmt ->execute();
-    $stmt_result = $stmt-> get_result();
 
-    if ($stmt_result->num_rows >0) {
-        $data = $stmt_result -> fetch_assoc();
-        if ($data['password'] == $password and $data['cpassword'] == $cpassword) {
-            header("Location:home.html");
-        }
-        else{
-            echo "Invalid";
-        }
-       
-    }
-    else{
-        echo "Invalid";
-    }
+$sql = ("update admin set password='$password', cpassword='$cpassword' where email='$email'");
+
+if ($conn->query($sql) === TRUE) {
+  header("Location:home.html");
+} else {
+  echo "Error updating record: " . $conn->error;
 }
+
+$conn->close();
+
+
+
 ?>
